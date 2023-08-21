dacdfd2cf99f056274d691b69523d43326c2ec41
protik0939 <protik0939@gmail.com>
2023-08-21 23:28:13 +0600

    W11 D4 Update

---
+++ Week 11/Day 4/Bangla_Numbers.cpp
@@ -0,0 +1,112 @@
+/*
+________________________________________________________________
+
+    ████████████████████████████████████████████████████████
+    █▄─▄▄─█▄─▄▄▀█─▄▄─█─▄─▄─█▄─▄█▄─█─▄███─▄▄─█░▄▄░█▄▄▄░█░▄▄░█
+    ██─▄▄▄██─▄─▄█─██─███─████─███─▄▀████─██─█▄▄▄░██▄▄░█▄▄▄░█
+    ▀▄▄▄▀▀▀▄▄▀▄▄▀▄▄▄▄▀▀▄▄▄▀▀▄▄▄▀▄▄▀▄▄▀▀▀▄▄▄▄▀▄▄▄▄▀▄▄▄▄▀▄▄▄▄▀
+________________________________________________________________
+
+*/
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+#define ll long long int
+#define somoy                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+#define tc    \
+    ll t;     \
+    cin >> t; \
+    while (t--)
+#define all(v) v.begin(), v.end()
+#define unq(v) v.erase(unique(all(v)), v.end())
+#define srt(v) sort(all(v))
+#define srtr(v) sort(v.rbegin(), v.rend())
+#define rvrs(v) reverse(all(v))
+#define pb push_back
+#define f(i, c, n) for (ll i = c; i < n; i++)
+#define fr(i, c, n) for (ll i = n - 1; i >= c; i--)
+#define f1(i, c, n) for (ll i = c; i <= n; i++)
+#define fr1(i, c, n) for (ll i = n; i > c; i--)
+#define vi vector<int>
+#define vl vector<ll>
+#define vs vector<string>
+#define vb vector<bool>
+#define vpi vector<pair<ll, ll>>
+#define vmax(v) *max_element(all(v))
+#define vmin(v) *min_element(all(v))
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pss pair<string, string>
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define fbo(x, n) *x.find_by_order(n)
+#define ook(x, n) x.order_of_key(n)
+
+void inWords(ll n)
+{
+    if (n >= 10000000)
+    {
+        inWords(n / 10000000);
+        cout << " kuti";
+        n %= 10000000;
+    }
+    if (n >= 100000)
+    {
+        inWords(n / 100000);
+        cout << " lakh";
+        n %= 100000;
+    }
+    if (n >= 1000)
+    {
+        inWords(n / 1000);
+        cout << " hajar";
+        n %= 1000;
+    }
+    if (n >= 100)
+    {
+        inWords(n / 100);
+        cout << " shata";
+        n %= 100;
+    }
+    if (n)
+    {
+        cout << " " << n;
+    }
+}
+
+void solve()
+{
+    ll n;
+    ll k = 1;
+
+    while (cin >> n)
+    {
+        cout.width(4);
+        cout << right << k++ << ".";
+
+        if (n == 0)
+        {
+            cout << " 0" << endl;
+            continue;
+        }
+
+        inWords(n);
+
+        cout << endl;
+    }
+}
+
+int main()
+{
+    somoy;
+    // tc
+    {
+        solve();
+    }
+}

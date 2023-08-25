ebd38fc4055b761c7aecd7892bb35a6314dee5a7
protik0939 <protik0939@gmail.com>
2023-08-25 23:46:21 +0600

    W12 D1 Update

---
+++ Week 12/Day 1/T_primes.cpp
@@ -0,0 +1,91 @@
+/*
+________________________________________________________________
+
+    ████████████████████████████████████████████████████████
+    █▄─▄▄─█▄─▄▄▀█─▄▄─█─▄─▄─█▄─▄█▄─█─▄███─▄▄─█░▄▄░█▄▄▄░█░▄▄░█
+    ██─▄▄▄██─▄─▄█─██─███─████─███─▄▀████─██─█▄▄▄░██▄▄░█▄▄▄░█
+    ▀▄▄▄▀▀▀▄▄▀▄▄▀▄▄▄▄▀▀▄▄▄▀▀▄▄▄▀▄▄▀▄▄▀▀▀▄▄▄▄▀▄▄▄▄▀▄▄▄▄▀▄▄▄▄▀
+________________________________________________________________
+
+*/
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+#define ll long long int
+#define somoy                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+#define tc    \
+    ll t;     \
+    cin >> t; \
+    while (t--)
+#define all(v) v.begin(), v.end()
+#define allr(v) v.rbegin(), v.rend()
+#define unq(v) v.erase(unique(all(v)), v.end())
+#define srt(v) sort(all(v))
+#define srtr(v) sort(v.rbegin(), v.rend())
+#define rvrs(v) reverse(all(v))
+#define pb push_back
+#define f(i, c, n) for (ll i = c; i < n; i++)
+#define fr(i, c, n) for (ll i = n - 1; i >= c; i--)
+#define f1(i, c, n) for (ll i = c; i <= n; i++)
+#define fr1(i, c, n) for (ll i = n; i > c; i--)
+#define vi vector<int>
+#define vl vector<ll>
+#define vs vector<string>
+#define vb vector<bool>
+#define vpi vector<pair<ll, ll>>
+#define vmax(v) *max_element(all(v))
+#define vmin(v) *min_element(all(v))
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pss pair<string, string>
+#define ys cout << "YES" << endl
+#define no cout << "NO" << endl
+#define ses cout << endl;
+#define sz(x) x.size()
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define fbo(x, n) *x.find_by_order(n)
+#define ook(x, n) x.order_of_key(n)
+ll w[1000000] = {0};
+void solve()
+{
+    w[1] = 1;
+    f1(i, 2, 1000)
+    {
+        for (ll j = 2 * i; j <= 1000000; j += i)
+        {
+            w[j] = 1;
+        }
+    }
+    ll n;
+    cin >> n;
+    while (n--)
+    {
+        ll x;
+        cin >> x;
+        ll n = (ll)sqrt(x);
+        if ((n * n) == x && w[n] == 0)
+        {
+            ys;
+        }
+        else
+        {
+            no;
+        }
+    }
+}
+
+int main()
+{
+    somoy;
+    // tc
+    {
+        solve();
+    }
+}
